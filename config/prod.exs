3239f09cbd0d627e8a15156de3d4ca1cff912a9d
Álvaro <alvaro.girona@gmail.com>
2024-05-12 00:31:51 +0200

    update config

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :lol_analytics_web, LoLAnalyticsWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "lol-analytics.kaizer.cloud", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
